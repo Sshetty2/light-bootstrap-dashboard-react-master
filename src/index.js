c0a039aa5ca449ebd247a47389c60cfee4a74028
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-01 23:29:59 -0400

    light bootstrap dashboard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { HashRouter, Route, Switch } from "react-router-dom";
+import  {HashRouter, Route, Switch} from "react-router-dom";
 
 import indexRoutes from "routes/index.jsx";
 
